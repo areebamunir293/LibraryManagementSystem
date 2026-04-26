2e1453eb07459f10bd9f40aa01457c8421a8eda0
Iyana-bit <iyana.june3@gmail.com>
2026-04-26 19:13:52 +0500

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include"features/bookmanagement.cpp"
+
+int main()
+{ 
+	//this is meant to test the functioning (not the final implementation)
+	bookmanager b1;
+	b1.menu(true);
+	return 0;
+}
